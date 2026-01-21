f31332d9eae64a06f72ccda4b1b2cfe83e7ac9e0
zekelkeys <zkwebdev@fastmail.com>
2026-01-21 14:13:35 -0800

    Add spacing/layout styles to checkboxes

--- src/components/Checkboxes.jsx
+++ src/components/Checkboxes.jsx
@@ -1,30 +1,39 @@
 function Checkboxes() {
     return (
-        <div className="mt-4 mb-10 lg:mb-12">
-            <label class="checkbox">
+        <div className="mt-4 mb-10 flex flex-col gap-3 md:flex-row md:gap-6 lg:mb-12">
+            <label className="flex cursor-pointer items-center gap-[10px]">
                 <input
+                    className="pointer-events-none"
                     id="excludeSpaces"
-                    class="checkbox__input"
                     type="checkbox"
                     name="excludeSpaces"
-                    autocomplete="off"
                 />
                 <span></span>
-                <span>Exclude Spaces</span>
+                <span className="trackiing-tiight text-base leading-tight">
+                    Exclude Spaces
+                </span>
             </label>
 
-            <label>
+            <label className="flex cursor-pointer items-center gap-[10px]">
                 <input
+                    className="pointer-events-none"
                     type="checkbox"
                     name="setCharacterLimit"
-                    autocomplete="off"
                 />
                 <span></span>
-                <span>Set Character Limit</span>
+                <span className="trackiing-tiight text-base leading-tight">
+                    Set Character Limit
+                </span>
             </label>
-            <div>
+
+            <div className="trackiing-tiight text-base leading-tight md:ml-auto">
                 Approx. reading time:{" "}
-                <output id="readingTime">0 minutes</output>
+                <output
+                    id="readingTime"
+                    className="trackiing-tiight text-base leading-tight"
+                >
+                    0 minutes
+                </output>
             </div>
         </div>
     );
