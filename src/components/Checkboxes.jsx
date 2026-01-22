b0445c714e46cbd4e815764986d4df775f46a80f
zekelkeys <zkwebdev@fastmail.com>
2026-01-21 21:35:40 -0800

    Add character-limit input styles

--- src/components/Checkboxes.jsx
+++ src/components/Checkboxes.jsx
@@ -17,7 +17,7 @@ function Checkboxes() {
                     <input
                         className="peer absolute cursor-pointer opacity-0"
                         type="checkbox"
-                        aria-controls="char-limit"
+                        aria-controls="character-limit"
                         aria-expanded="{enabled}"
                     />
                     <span className="h-4 w-4 rounded-xs border-1 border-neutral-900 bg-transparent bg-cover bg-center bg-no-repeat group-hover:border-purple-500 peer-checked:border-purple-400 peer-checked:bg-purple-400 peer-checked:bg-[url('./assets/images/icon-check.svg')] group-hover:peer-checked:border-purple-500 group-hover:peer-checked:bg-purple-500 peer-focus-visible:border-neutral-200 peer-focus-visible:bg-white peer-focus-visible:shadow-[0_0_0_0.125rem_#FFFFFF,0_0_0_0.25rem_#D3A0FA] peer-checked:peer-focus-visible:border-purple-400 peer-focus-visible:peer-checked:bg-purple-400 dark:border-neutral-200"></span>
@@ -27,12 +27,12 @@ function Checkboxes() {
                 </label>
 
                 <div>
-                    <label className="sr-only" htmlFor="char-limit">
+                    <label className="sr-only" htmlFor="character-limit">
                         Character limit
                     </label>
                     <input
                         className="ml-2.5 h-[1.813rem] w-[3.438rem] [appearance:textfield] rounded-sm border-1 border-neutral-600 px-3 py-1 text-base leading-tight tracking-tight text-neutral-900 dark:text-white [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-                        id="char-limit"
+                        id="character-limit"
                         type="number"
                     />
                 </div>
