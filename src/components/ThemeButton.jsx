eea97011dbe0b83c660faff67d30cab5bbc81199
zekelkeys <zkwebdev@fastmail.com>
2026-01-22 08:45:09 -0800

    Add hover styles to themeButton

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -18,7 +18,7 @@ function ThemeButton() {
     }
     return (
         <button
-            className="grid h-8 w-8 cursor-pointer place-items-center rounded-sm bg-neutral-100 bg-[url('./assets/images/icon-moon.svg')] bg-cover bg-size-[1.25rem] bg-center bg-no-repeat md:h-11 md:w-11 md:rounded-md md:bg-size-[1.375rem] dark:bg-neutral-700 dark:bg-[url('./assets/images/icon-sun.svg')]"
+            className="grid h-8 w-8 cursor-pointer place-items-center rounded-sm bg-neutral-100 bg-[url('./assets/images/icon-moon.svg')] bg-cover bg-size-[1.25rem] bg-center bg-no-repeat hover:bg-neutral-200 md:h-11 md:w-11 md:rounded-md md:bg-size-[1.375rem] dark:bg-neutral-700 dark:bg-[url('./assets/images/icon-sun.svg')] dark:hover:bg-neutral-600"
             onClick={toggleTheme}
             type="button"
             aria-label={
