aa39a60c29d433fcba6e00f92ba61054be8d9533
zekelkeys <zkwebdev@fastmail.com>
2026-01-22 09:22:23 -0800

    Add focus styles for themeButton

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -18,7 +18,7 @@ function ThemeButton() {
     }
     return (
         <button
-            className="grid h-8 w-8 cursor-pointer place-items-center rounded-sm bg-neutral-100 bg-[url('./assets/images/icon-moon.svg')] bg-cover bg-size-[1.25rem] bg-center bg-no-repeat hover:bg-neutral-200 md:h-11 md:w-11 md:rounded-md md:bg-size-[1.375rem] dark:bg-neutral-700 dark:bg-[url('./assets/images/icon-sun.svg')] dark:hover:bg-neutral-600"
+            className="grid h-8 w-8 cursor-pointer place-items-center rounded-sm bg-neutral-100 bg-[url('./assets/images/icon-moon.svg')] bg-cover bg-size-[1.25rem] bg-center bg-no-repeat hover:bg-neutral-200 focus-visible:shadow-[0_0_0.625rem_#C27CF8] focus-visible:ring-2 focus-visible:ring-purple-500 focus-visible:outline-none md:h-11 md:w-11 md:rounded-md md:bg-size-[1.375rem] dark:bg-neutral-700 dark:bg-[url('./assets/images/icon-sun.svg')] dark:hover:bg-neutral-600"
             onClick={toggleTheme}
             type="button"
             aria-label={
