8b0f7f1931dd19de1d53cb15565544572d002ee9
zekelkeys <zkwebdev@fastmail.com>
2026-01-24 10:01:35 -0800

    Delete Wrapper component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
 import "../index.css";
+import TextStatsContextProvider from "../context/TextStatsContext";
 import Logo from "./Logo";
 import ThemeButton from "./ThemeButton";
-import Wrapper from "./Wrapper";
 import TextArea from "./TextArea";
-import Checkboxes from "./Checkboxes";
 import StatsCards from "./StatsCards";
-import TextStatsContextProvider from "../context/TextStatsContext";
+import ExcludeSpaces from "./ExcludeSpaces";
+import CharacterLimit from "./CharacterLimit";
+import ReadingTime from "./ReadingTime";
 
 function App() {
     return (
-        <Wrapper>
+        <div className="min-h-screen w-full bg-[url('./assets/images/bg-light-theme.png')] bg-cover bg-center bg-no-repeat px-4 pt-4 font-dm-sans md:px-8 md:pt-8 dark:bg-[url('./assets/images/bg-dark-theme.png')]">
             <div className="m-auto max-w-[61.875rem]">
                 <header className="flex items-center justify-between">
                     <Logo />
@@ -22,12 +22,16 @@ function App() {
                     </h1>
                     <TextStatsContextProvider>
                         <TextArea />
-                        <Checkboxes />
+                        <div className="mt-4 mb-10 flex flex-col gap-3 text-neutral-900 md:flex-row md:gap-6 lg:mb-12 dark:text-neutral-200">
+                            <ExcludeSpaces />
+                            <CharacterLimit />
+                            <ReadingTime />
+                        </div>
                         <StatsCards />
                     </TextStatsContextProvider>
                 </main>
             </div>
-        </Wrapper>
+        </div>
     );
 }
 
